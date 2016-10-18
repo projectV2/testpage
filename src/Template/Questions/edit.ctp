6eacfca63fb05b0c3dea13a421ad84c386449e26
Ubuntu <ubuntu@ip-172-31-27-5.eu-central-1.compute.internal>
2016-10-18 18:50:35 +0000

    Initial commit. Questions page does work.

---
+++ src/Template/Questions/edit.ctp
@@ -0,0 +1,22 @@
+<nav class="large-3 medium-4 columns" id="actions-sidebar">
+    <ul class="side-nav">
+        <li class="heading"><?= __('Actions') ?></li>
+        <li><?= $this->Form->postLink(
+                __('Delete'),
+                ['action' => 'delete', $question->id],
+                ['confirm' => __('Are you sure you want to delete # {0}?', $question->id)]
+            )
+        ?></li>
+        <li><?= $this->Html->link(__('List Questions'), ['action' => 'index']) ?></li>
+    </ul>
+</nav>
+<div class="questions form large-9 medium-8 columns content">
+    <?= $this->Form->create($question) ?>
+    <fieldset>
+        <legend><?= __('Edit Question') ?></legend>
+        <?php
+        ?>
+    </fieldset>
+    <?= $this->Form->button(__('Submit')) ?>
+    <?= $this->Form->end() ?>
+</div>
---
+++ tests/Fixture/QuestionsFixture.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * QuestionsFixture
+ *
+ */
+class QuestionsFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 6, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'latin1_swedish_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 1
+        ],
+    ];
+}
